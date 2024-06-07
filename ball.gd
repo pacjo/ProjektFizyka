231e0b1746eae61aaa5868d9c97c15a04f59f314
pacjo <kamil.kroliczek2004@gmail.com>
2024-06-07 19:24:13 +0200

    add experimental touch support

--- ball.gd
+++ ball.gd
@@ -2,6 +2,8 @@ extends RigidBody2D
 
 var arrow: Line2D
 var is_dragging: bool = false
+var wasShoot: bool = false
+@export var health: float = 10
 
 const MIN_DRAG_DISTANCE = 30
 const MAX_ARROW_LENGTH = 150
@@ -11,45 +13,73 @@ func _ready():
 	arrow = $Line2D
 	arrow.visible = false
 
+func _process(delta):
+	# delta is in seconds and by substracting it from health (by default
+	# being a value of 10) ball object will live 10 seconds
+	# ...and we only do that if it was already shoot (wasShoot)
+	if wasShoot:
+		health -= delta
+
+	if health <= 0:
+		# TODO: add animation (maybe a disappearing cloud?)
+		free()
+
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
+	if !wasShoot:
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				if _is_mouse_over_ball(event.global_position):
+				# we started dragging the ball - show arrow and calculate stuff
+				if _is_over_ball(event.global_position):
 					is_dragging = true
-					arrow.visible = false  # Start with arrow hidden until we check distance
-					handle_event(event.global_position)  # Update arrow initially to ensure correct visibility
+					draw_arrow(event.global_position)
 			else:
+				# we stopped dragging the ball - throw it
 				is_dragging = false
 				if arrow.visible:
 					apply_velocity(event.global_position)
 				arrow.visible = false
-	elif event is InputEventMouseMotion and is_dragging:
-		handle_event(event.global_position)
+		elif event is InputEventMouseMotion and is_dragging:
+			draw_arrow(event.global_position)
+		elif event is InputEventScreenTouch:
+			if event.pressed:
+				# we started dragging the ball - show arrow and calculate stuff
+				if _is_over_ball(event.position):
+					is_dragging = true
+					draw_arrow(event.position)
+			else:
+				# we stopped dragging the ball - throw it
+				is_dragging = false
+				if arrow.visible:
+					apply_velocity(event.position)
+				arrow.visible = false
+		elif event is InputEventScreenDrag and is_dragging:
+			draw_arrow(event.position)
 
-func _is_mouse_over_ball(mouse_position):
+func _is_over_ball(pointer_position):
 	var space_state = get_world_2d().direct_space_state
 	var query = PhysicsPointQueryParameters2D.new()
-	query.position = mouse_position
+	
+	query.position = pointer_position
 	var result = space_state.intersect_point(query)
+	
 	for item in result:
 		if item.collider == self:
 			return true
 	return false
 
-func handle_event(mouse_position):
-	var distance = global_position.distance_to(mouse_position)
-	# Draw arrow
+func draw_arrow(position):
+	var distance = global_position.distance_to(position)
 	if distance < MIN_DRAG_DISTANCE:
 		arrow.visible = false
 		return
 	
-	# TODO: fix arrow movement when ball is rotating
-	var direction = (global_position - mouse_position).normalized()
+	var direction = (global_position - position).normalized()
 	distance = min(distance, MAX_ARROW_LENGTH)
 	arrow.points = [Vector2.ZERO, direction * distance]
 	arrow.visible = true
 
-func apply_velocity(mouse_position):
-	var direction = (global_position - mouse_position).normalized()
+func apply_velocity(position):
+	var direction = (global_position - position).normalized()
 	linear_velocity = direction * VELOCITY_MULTIPLIER
+	
+	wasShoot = true
