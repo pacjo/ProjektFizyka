c43b9ad8dfd079d558dad98df69492e219db66ff
pacjo <kamil.kroliczek2004@gmail.com>
2024-06-24 14:37:03 +0200

    fix destruction animation

--- scripts/Breakable.gd
+++ scripts/Breakable.gd
@@ -28,14 +28,20 @@ func _on_body_shape_entered(_body_rid, body, _body_shape_index, _local_shape_ind
 		health -= damage
 		
 		if health <= 0:
+			call_deferred("set_contact_monitor", false)
+			
 			play_destruction_animation()
 			ScoreManager.add_score(score_points)
 			preserve_ball_velocity(body)
 
 func preserve_ball_velocity(ball):
 	ball.linear_velocity = ball.current_velocity * post_collision_speed_multiplier
-	queue_free()
 
 func play_destruction_animation():
-	print("animation played")
+	#print(name, " - will play animation, status: ", $AnimationPlayer.is_playing())
 	$AnimationPlayer.play("destruction")
+	#print(name, " - animation played, status: ", $AnimationPlayer.is_playing(), ", ", $AnimationPlayer.current_animation)
+
+func _on_animation_finished(anim_name):
+	if anim_name == "destruction":
+		queue_free()
